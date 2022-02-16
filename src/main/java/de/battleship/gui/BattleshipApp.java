eff6e9062067799cf63c312db3dbda7a61908bcf
nxwithekid <moritz.nowi2000@gmail.com>
2022-02-16 12:27:44 +0100

    FXML 1. Fenster + bild

--- src/main/java/de/battleship/gui/BattleshipApp.java
+++ src/main/java/de/battleship/gui/BattleshipApp.java
@@ -1,13 +1,17 @@
 package de.battleship.gui;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import javax.print.attribute.standard.Media;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ResourceBundle;
 
 public class BattleshipApp extends Application {
     private static Stage primaryStage;
@@ -26,9 +30,12 @@ public class BattleshipApp extends Application {
         URL res = getClass().getResource("battleship.fxml");
         Parent root = FXMLLoader.load(res);
 
+
         primaryStage.setTitle("Battleship");
         primaryStage.setScene(new Scene(root, 790, 400));
         primaryStage.setResizable(false);
         primaryStage.show();
     }
+
+
 }
