c22580d9e09c72f69df4cf9a0e88df6d2effef12
lanabrandscher <lana.brandscher@gmail.com>
2022-02-16 14:17:44 +0100

    Tests erweitert

--- src/test/java/de/battleship/service/ServiceTest.java
+++ src/test/java/de/battleship/service/ServiceTest.java
@@ -1,14 +1,11 @@
 package de.battleship.service;
 
-import de.battleship.dao.BackendDAO;
-import de.battleship.dao.BackendDAOImp;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ServiceTest {
     static int[][] testFeld = {{1, 0, 0, 0, 0, 1, 1, 0, 0, 0},
@@ -24,89 +21,73 @@ public class ServiceTest {
 
     static SpielFeldService spielFeldService = new SpielFeldServiceImp();
     BSSocket bs;
-    BackendDAO backendDAO  = new BackendDAO() {
 
-        @Override
-        public boolean istValide(int[][] feld, int anzahlBattleship, int anzahlCruiser, int anzahlDestroyer, int anzahlSubmarine) {
-            return false;
-        }
-
-        @Override
-        public boolean istGetroffen(int x, int y) {
-            return false;
-        }
-
-        @Override
-        public boolean schonMalGeschossen() {
-            return false;
-        }
-
-        @Override
-        public boolean istVersenkt(int x, int y) {
-            return false;
-        }
-
-        @Override
-        public boolean spielBeendet() {
-            return false;
-        }
-
-        @Override
-        public List<int[]> getLetztesVersenktesSchiff() {
-            return null;
-        }
-
-        @Override
-        public void setFeld(int[][] feld) {
-
-        }
-
-        @Override
-        public List<List<int[]>> getSchiffe() {
-            return null;
-        }
-
-        @Test
-        void testValide() {
-            boolean check = spielFeldService.istValide(testFeld);
-            assertTrue(check);
-        }
-    };
-
+    @BeforeEach
+    void init() {
+        spielFeldService.istValide(testFeld);
+    }
 
     @Test
-    void testZugAbschicken () throws IOException {
+    void testZugAbschicken() throws IOException {
         int x = 0;
         int y = 0;
-        Zug zug = new Zug (x,y);
-        bs = new BSSocket(spielFeldService,"localhost", 22000, 22001);
-        boolean checkAbschicken = spielFeldService.zugAbschicken(x,y);
+        Zug zug = new Zug(x, y);
+        bs = new BSSocket(spielFeldService, "localhost", 22000, 22001);
+        boolean checkAbschicken = spielFeldService.zugAbschicken(x, y);
         assertTrue(checkAbschicken);
     }
 
     @Test
-    void testAntwortErstellen(Zug zug)
-    {
-        int x = zug.getX();
-        int y = zug.getY();
-        Antwort antwort = new Antwort();
-        boolean checkAntwortErstellen(spielFeldService.antwortErstellen());
-        if (backendDAO.istGetroffen(x,y))
-        {
-            antwort.setGetroffen(true);
-            assertTrue(checkAntwortErstellen);
-        }
-        if (backendDAO.istVersenkt(x,y))
-        {
-            antwort.setVersenkt(true);
-            assertTrue(checkAntwortErstellen);
-            antwort.setKoordinaten(backendDAO.getLetztesVersenktesSchiff());
-        }
-        if (backendDAO.spielBeendet())
-        {
-            antwort.setSpielBeendet(true);
-        }
+    void testAntwortErstellenGetroffen() {
+        Zug zug = new Zug(0, 0);
+
+        Antwort antwort = spielFeldService.antwortErstellen(zug);
+        assertTrue(antwort.isGetroffen());
     }
 
+    @Test
+    void testAntwortErstellenVersenkt() {
+        Zug zug = new Zug(8, 7);
+
+        Antwort antwort = spielFeldService.antwortErstellen(zug);
+        assertTrue(antwort.isVersenkt());
+    }
+
+    @Test
+    void testAntwortErstellenSchonGetroffen() {
+        Zug zug = new Zug(8, 7);
+        Zug zug1 = new Zug(8, 7);
+
+        Antwort antwort = spielFeldService.antwortErstellen(zug);
+        Antwort antwort1 = spielFeldService.antwortErstellen(zug1);
+        assertTrue(antwort.isVersenkt());
+        assertTrue(antwort1.isSchonMalGeschossen());
+    }
+
+    @Test
+    void testAntwortErstellenBeenden() {
+        spielFeldService.antwortErstellen(new Zug(0, 0));
+        spielFeldService.antwortErstellen(new Zug(1, 0));
+        spielFeldService.antwortErstellen(new Zug(2, 0));
+        spielFeldService.antwortErstellen(new Zug(3, 0));
+        spielFeldService.antwortErstellen(new Zug(1, 2));
+        spielFeldService.antwortErstellen(new Zug(2, 2));
+        spielFeldService.antwortErstellen(new Zug(7, 3));
+        spielFeldService.antwortErstellen(new Zug(2, 4));
+        spielFeldService.antwortErstellen(new Zug(5, 4));
+        spielFeldService.antwortErstellen(new Zug(0, 5));
+        spielFeldService.antwortErstellen(new Zug(2, 5));
+        spielFeldService.antwortErstellen(new Zug(5, 5));
+        spielFeldService.antwortErstellen(new Zug(0, 6));
+        spielFeldService.antwortErstellen(new Zug(2, 6));
+        spielFeldService.antwortErstellen(new Zug(5, 6));
+        spielFeldService.antwortErstellen(new Zug(1, 8));
+        spielFeldService.antwortErstellen(new Zug(2, 8));
+        spielFeldService.antwortErstellen(new Zug(4, 8));
+        spielFeldService.antwortErstellen(new Zug(6, 8));
+        Antwort antwort = spielFeldService.antwortErstellen(new Zug(8, 7));
+
+        assertTrue(antwort.isSpielBeendet());
+    }
 }
 
